163aa435a38b5059c4f7d642b0663a94582ffd78
agershkovich <agershkovich@lohika.com>
2019-04-22 01:23:25 +0300

    Phase_2 revert env var

--- main.py
+++ main.py
@@ -13,25 +13,23 @@ from src.xml_lib import *
 
 
 def main():
-    tree = get_tree_of_xml_file('samples/input/test_data.xml')
+    tree = get_tree_of_xml_file(input_xml_file)
     root = tree.getroot()
-    child = root[1]
-    data_from_csv = read_csv('samples/input/input_data.csv')
+    child = root[index_of_child]
+    data_from_csv = read_csv(input_csv_file)
 
     for previous_value_from_xml in child:
         for new_value_from_csv in data_from_csv:
             if previous_value_from_xml.tag == new_value_from_csv[0]:
                 previous_value_from_xml.text = new_value_from_csv[1]
 
-    tree.write('samples/output/output_test_data.xml')
+    tree.write(output_xml_file)
 
-    tree.write('samples/output/output_test_data.xml', xml_declaration=True,
-               encoding='UTF-8')
+    tree.write(output_xml_file, xml_declaration=True, encoding='UTF-8')
 
-    xml_to_json('samples/output/output_test_data.xml', 'samples/output/output_test_data.json')
+    xml_to_json(output_xml_file, output_json_file)
 
-    json_to_xml('samples/output/output_test_data.json',
-                'samples/output/output_json_to_xml_test_data.xml')
+    json_to_xml(output_json_file, output_json_to_xml_file)
 
 
 if __name__ == "__main__":
--- test/xml_json_test.py
+++ test/xml_json_test.py
@@ -4,24 +4,24 @@ from src.xml_lib import *
 
 
 def test_if_input_xml_file_exist():
-    assert os.path.isfile(os.path.abspath('samples/input/test_data.xml')) is True
+    assert os.path.isfile(os.path.abspath(input_xml_file)) is True
 
 
 def test_if_input_xml_file_has_correct_structure():
-    assert validate('samples/input/test_data.xml', 'samples/input/test_data.xsd') is True
+    assert validate(input_xml_file, input_xsd_file) is True
 
 
 def test_if_output_xml_file_exist():
-    assert os.path.isfile(os.path.abspath('samples/output/output_test_data.xml')) is True
+    assert os.path.isfile(os.path.abspath(output_xml_file)) is True
 
 
 def test_if_output_xml_file_has_correct_structure():
-    assert validate('samples/output/output_test_data.xml', 'samples/input/test_data.xsd') is True
+    assert validate(output_xml_file, input_xsd_file) is True
 
 
 def test_if_output_json_file_exist():
-    assert os.path.isfile(os.path.abspath('samples/output/output_test_data.json')) is True
+    assert os.path.isfile(os.path.abspath(output_json_file)) is True
 
 
 def test_if_json_file_content_is_correct():
-    assert compare_xml('samples/output/output_test_data.xml', 'samples/output/output_json_to_xml_test_data.xml') is ''
+    assert compare_xml(output_xml_file, output_json_to_xml_file) is ''
