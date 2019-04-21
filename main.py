34b8d559218567a818dd0d72414f4bafc87a40a0
agershkovich <agershkovich@lohika.com>
2019-04-21 23:57:02 +0300

    Add env to main

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from src.xml_lib import *
 
+env_from_csv = read_csv('samples/input/env.csv')
+
+input_xml_file = env_from_csv[0][1]
+output_xml_file = env_from_csv[1][1]
+input_csv_file = env_from_csv[2][1]
+index_of_child = int(env_from_csv[3][1])
+output_json_file = env_from_csv[4][1]
+input_xsd_file = env_from_csv[5][1]
+output_json_to_xml_file = env_from_csv[6][1]
+
 
 def main():
     tree = get_tree_of_xml_file(input_xml_file)
